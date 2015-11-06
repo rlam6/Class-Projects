3b968ea19e4dc60868d5433b33c8d1dad40b7249
Christian Morte <cmorte@uci.edu>
2015-11-06 12:28:27 -0800

    Create listHelper.hpp

---
+++ Project-2---Final-Grades-Checker/listHelper.hpp
@@ -0,0 +1,10 @@
+#ifndef LIST_HELPER_HPP
+#define LIST_HELPER_HPP
+
+#import <string>
+
+void fillIntList(int len, int* array);
+
+void fillStrList(int len, std::string* array);
+
+#endif //LIST_HELPER_HPP
