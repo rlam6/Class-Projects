014ee3b876ec8d9a5605bd090a92365284a95d73
Christian Morte <cmorte@uci.edu>
2015-11-06 12:18:15 -0800

    Create mathHelper.hpp

---
+++ Project-1---Distance-Calculations/mathHelper.hpp
@@ -0,0 +1,12 @@
+#ifndef MATH_HELPER_HPP
+#define MATH_HELPER_HPP
+
+const double pi = 3.14159;
+
+double radian(double n);
+
+double absVal(double n);
+
+double squared(double n);
+
+#endif // MATH_HELPER_HPP
