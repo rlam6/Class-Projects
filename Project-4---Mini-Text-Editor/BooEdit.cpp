e444c02f535357e23a007befef4624ca4729ef3b
Christian Morte <cmorte@uci.edu>
2015-12-12 18:50:52 -0800

    Create BooEdit.cpp

---
+++ Project-4---Mini-Text-Editor/BooEdit.cpp
@@ -0,0 +1,24 @@
+
+// Implementation of the BooEdit class
+//
+// DO NOT MODIFY THIS FILE
+// THIS FILE BELONGS TO THE ICS45C INSTRUCTOR - ALEX THRONTON
+
+#include "BooEdit.hpp"
+#include "InteractionProcessor.hpp"
+
+
+void BooEdit::run()
+{
+    view.start();
+
+    InteractionProcessor processor{model, view, interactionReader};
+    processor.run();
+}
+
+
+BooEdit::~BooEdit()
+{
+    view.stop();
+}
+
