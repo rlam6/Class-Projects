467af7a20a4456a41ddd8195ed01b3b5c2a462a0
Christian Morte <cmorte@uci.edu>
2015-12-12 19:31:42 -0800

    Update InteractionType.hpp

--- Project-4---Mini-Text-Editor/InteractionType.hpp
+++ Project-4---Mini-Text-Editor/InteractionType.hpp
@@ -5,13 +5,6 @@
 #ifndef INTERACTIONTYPE_HPP
 #define INTERACTIONTYPE_HPP
 
-
-
-// An "enum class" defines a type that has one of the constant values specified,
-// but can't have any other values.  So, in this case, a variable of type
-// InteractionType could have the value InteractionType::command,
-// InteractionType::undo, and so on.
-
 enum class InteractionType
 {
     command,
